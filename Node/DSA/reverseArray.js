3ce2ca27f22e4d2b1830fccc31d76806d0ab6c53
Rutuja Sontakke <rutujasontakke50@gmail.com>
2026-09-25 19:31:33 +0530

    Update reverseArray function with new test array and clean up code

--- Node/DSA/reverseArray.js
+++ Node/DSA/reverseArray.js
@@ -1,22 +1,19 @@
-const arr = [5,4,3,2,1,7,5,8,4,10,-100];
+const arr = [5,3,2,1,3,0];
 
 function reverseArray(arr) {
+    let left = 0;
+    let right = arr.length - 1;
 
-    let left = 0 ; //left start always with 0
-    let right = arr.length - 1; //right always start with arr.length - 1
-
-    while (left < right) {
-        let temp = arr[left]; //for swapping
-        arr[left] = arr[right];
-        arr[right]= temp;
+    while(left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right]
+        arr[right] = temp;
 
         left ++;
         right --;
-
     }
-    return arr;
 
+    return arr;
 }
 
 console.log(reverseArray(arr));
-console.log(JSON.stringify(reverseArray(arr)))
